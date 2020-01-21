e3ce9334047c32a1325ebeedd19a7d6d38ec5488
Akash Das <mfs.akash@gmail.com>
2020-01-21 14:54:39 +0530

    Added all the files and dependencies

---
+++ js/resume.js
@@ -0,0 +1,8 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+})(jQuery); // End of use strict
